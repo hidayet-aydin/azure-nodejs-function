c07c1c231e7bc6a5a1e13992058972881642338b
hidayet-aydin <aydin.hid@gmail.com>
2021-09-30 21:49:41 +0300

    using graphql-weather-api with graphql-request

--- testPoint/index.js
+++ testPoint/index.js
@@ -1,16 +1,65 @@
-const singleFunction = async (context, req) => {
+const { GraphQLClient, gql } = require("graphql-request");
+
+const testPoint = async (context, req) => {
   context.log("JavaScript HTTP trigger function processed a request.");
 
-  const name = req.query.name || (req.body && req.body.name);
-  
-  const responseMessage = name
-    ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-    : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+  if (req.method !== "POST") {
+    return (context.res = {
+      status: 403,
+      body: "Forbidden request (only allow POST request)",
+    });
+  }
 
-  context.res = {
-    // status: 200, /* Defaults to 200 */
-    body: responseMessage,
-  };
+  const cityName = req.query.cityName || (req.body && req.body.cityName);
+  if (!cityName) {
+    return (context.res = {
+      status: 405,
+      body: "Forgotten city name! {cityName:'istanbul'}",
+    });
+  }
+
+  try {
+    const graphQLClient = new GraphQLClient(
+      "https://graphql-weather-api.herokuapp.com/"
+    );
+    const query = gql`
+    {
+      getCityByName(name: "${cityName}") {
+        name
+        country
+        weather {
+          summary {
+            title
+            description
+          }
+          temperature {
+            actual
+            feelsLike
+            min
+            max
+          }
+          timestamp
+        }
+      }
+    }
+    `;
+    const data = await graphQLClient.request(query);
+    if (!data.getCityByName) {
+      return (context.res = {
+        status: 404,
+        body: "Not found city name!",
+      });
+    }
+    return (context.res = {
+      status: 200,
+      body: JSON.stringify(data, null, 2),
+    });
+  } catch (error) {
+    return (context.res = {
+      status: 500,
+      body: error,
+    });
+  }
 };
 
-module.exports = singleFunction;
+module.exports = testPoint;
